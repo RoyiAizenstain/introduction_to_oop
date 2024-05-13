1db80bc6ba20d0f07327e0f4a8a9489ea7917efd
Royi Aizenstain <Royi.aizen@gmail.com>
2024-05-13 19:49:58 +0300

    assignment2 - Done!

--- src/assignment2/GaussianElimination.java
+++ src/assignment2/GaussianElimination.java
@@ -6,40 +6,40 @@ public class GaussianElimination {
          * Perform Gaussian elimination on the input matrix
          * @param matrix input matrix
          */
-        int h = 0;
-        int k = 0;
+        int h = 1;
+        int k = 1;
 
         int m = matrix.length;
         int n = matrix[0].length;
 
-        while (h < m && k < n) {
+        while (h <= m && k <= n) {
             // Find the k-th pivot
-            int i_max = h;
-            for (int i = h + 1; i < m; i++) {
-                if (Math.abs(matrix[i][k]) > Math.abs(matrix[i_max][k])) {
+            int i_max = h - 1;
+            for (int i = h; i < m; i++) {
+                if (Math.abs(matrix[i][k - 1]) > Math.abs(matrix[i_max][k - 1])) {
                     i_max = i;
                 }
             }
 
-            if (matrix[i_max][k] == 0) {
+            if (matrix[i_max][k - 1] == 0) {
                 // No pivot in this column, pass to next column
                 k++;
             } else {
                 // Swap rows h and i_max
-                double[] temp = matrix[h];
-                matrix[h] = matrix[i_max];
+                double[] temp = matrix[h - 1];
+                matrix[h - 1] = matrix[i_max];
                 matrix[i_max] = temp;
 
                 // Do for all rows below pivot
-                for (int i = h + 1; i < m; i++) {
-                    double f = matrix[i][k] / matrix[h][k];
+                for (int i = h; i < m; i++) {
+                    double f = matrix[i][k - 1] / matrix[h - 1][k - 1];
 
                     // Fill with zeros the lower part of pivot column
-                    matrix[i][k] = 0;
+                    matrix[i][k - 1] = 0;
 
                     // Do for all remaining elements in current row
-                    for (int j = k + 1; j < n; j++) {
-                        matrix[i][j] -= matrix[h][j] * f;
+                    for (int j = k; j < n; j++) {
+                        matrix[i][j] -= matrix[h - 1][j] * f;
                     }
                 }
 
