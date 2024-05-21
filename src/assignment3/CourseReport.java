f38b4613f83010516a693258ffdf5cddeed569f3
Royi <royi.aizen@gmail.com>
2024-05-21 17:31:40 +0300

    assigment 3 - In Progress!

---
+++ src/assignment3/CourseReport.java
@@ -0,0 +1,65 @@
+package assignment3;
+
+// This class represents a course report.
+public class CourseReport {
+    // Default constructor
+    public CourseReport() {
+        this.name = null;
+        this.points = 0;
+        this.grade = 0;
+    }
+
+    // Constructor
+    public CourseReport(String name, int points, double grade) {
+        this.name = name;
+        this.points = points;
+        this.grade = grade;
+    }
+
+    // Copy constructor
+    public CourseReport(CourseReport other) {
+        this.name = other.name;
+        this.points = other.points;
+        this.grade = other.grade;
+    }
+
+    // Getters and setters
+    public double getGrade() {
+        return this.grade;
+    }
+
+    public void setGrade(double grade) {
+        // Ensure grade is between 0 and 100
+        if (grade < 0)
+            grade = 0;
+        else if (grade > 100)
+            grade = 100;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        // Return the name of the course
+        return this.name;
+    }
+
+    public void setName(String name) {
+        // Set the name of the course
+        this.name = name;
+    }
+
+    public int getPoints() {
+        // Return the points of the course
+        return this.points;
+    }
+
+    public void setPoints(int points) {
+        // Set the points of the course
+        if (points >= 0)
+            this.points = points;
+    }
+
+    // Instance fields
+    private String name;
+    private int points;
+    private double grade;
+}
---
+++ src/assignment3/Student.java
@@ -0,0 +1,87 @@
+package assignment3;
+
+// This class represents a student.
+public class Student {
+    // Default constructor
+    public Student() {
+        this.name = null;
+        this.id = 0;
+        this.courseReports = null;
+    }
+
+    // Constructor
+    public Student(String name, int id, int numOfCourses) {
+        this.name = name;
+        this.id = id;
+        this.courseReports = new CourseReport[numOfCourses];
+    }
+
+    // Copy constructor
+    public Student(Student other) {
+        this.name = other.name;
+        this.id = other.id;
+        // Copy course reports
+        this.courseReports = new CourseReport[other.courseReports.length];
+        // Iterate over all course reports
+        for (int i = 0; i < other.courseReports.length; i++) {
+            // If the course report is not null, copy it
+            if (other.courseReports[i] != null)
+                this.courseReports[i] = new CourseReport(other.courseReports[i]);
+        }
+    }
+
+    // Getters and setters
+    // Get the name of the student
+    public String getName() {
+        return this.name;
+    }
+
+    // Set the name of the student
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Get the ID of the student
+    public int getId() {
+        return this.id;
+    }
+
+    // Set the ID of the student
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Get the course reports of the student
+    public CourseReport[] getCourseReports() {
+        return courseReports;
+    }
+
+    // Add a course report to the student
+    public void addCourse(CourseReport courseReport) {
+        for (int i = 0; i < courseReports.length; i++) {
+            if (courseReports[i] == null) {
+                courseReports[i] = new CourseReport(courseReport);
+                break;
+            }
+        }
+    }
+
+    // Get the weighted average of the student
+    public double getWeightedAverage() {
+        double sum = 0;
+        double totalPoints = 0;
+        // Iterate over all course reports
+        for (int i = 0; i < courseReports.length; i++) {
+            if (courseReports[i] != null) {
+                sum += courseReports[i].getGrade() * courseReports[i].getPoints();
+                totalPoints += courseReports[i].getPoints();
+            }
+        }
+        return sum / totalPoints;
+    }
+
+    // Instance fields
+    private String name;
+    private int id;
+    private CourseReport[] courseReports;
+}
