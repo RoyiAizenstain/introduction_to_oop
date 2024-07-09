71b93239d580a6098b6bd4f588c7c91000f62005
Royi <royi.aizen@gmail.com>
2024-07-09 15:43:39 +0300

    assigment 9 - Done!

--- src/assignment9/Map.java
+++ src/assignment9/Map.java
@@ -1,48 +1,62 @@
 package assignment9;
 
+// This class represents a map that stores key-value pairs.
 public class Map<K extends Comparable<K>, V> {
-    private final BinarySearchTree<Entry<K, V>> bst;
+    // The binary search tree
+    private final BinarySearchTree<CompEntry<K, V>> bst;
 
+    // Constructor
     public Map() {
         bst = new BinarySearchTree<>();
     }
 
+    // Add a key-value pair to the map
     public boolean add(K key, V value) {
-        return bst.add(new Entry<>(key, value));
+        return bst.add(new CompEntry<>(key, value));
     }
 
+    // Get the value associated with a key
     public V get(K key) {
-        Entry<K, V> entry = bst.get(new Entry<>(key, null));
-        return entry != null ? entry.getValue() : null;
+        // Get the entry with the key
+        CompEntry<K, V> compEntry = bst.get(new CompEntry<>(key, null));
+        return compEntry != null ? compEntry.getValue() : null;
     }
 
+    // Get the size of the map
     public int size() {
         return bst.size();
     }
 
+    // Check if the map contains a key
     public boolean contains(K key) {
-        return bst.contains(new Entry<>(key, null));
+        return bst.contains(new CompEntry<>(key, null));
     }
 
-    public static class Entry<K extends Comparable<K>, V> implements Comparable<Entry<K, V>> {
+    // This class represents a key-value pair that is comparable by the key
+    public static class CompEntry<K extends Comparable<K>, V> implements Comparable<CompEntry<K, V>> {
+        // The key and value
         private K key;
         private V value;
 
-        public Entry(K key, V value) {
+        // Constructor
+        public CompEntry(K key, V value) {
             this.key = key;
             this.value = value;
         }
 
+        // Get the key
         public K getKey() {
             return key;
         }
 
+        // Get the value
         public V getValue() {
             return value;
         }
 
+        // Set the value
         @Override
-        public int compareTo(Entry<K, V> other) {
+        public int compareTo(CompEntry<K, V> other) {
             return this.key.compareTo(other.getKey());
         }
     }
